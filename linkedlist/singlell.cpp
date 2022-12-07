f57c52283922f8466b182b8633271b2da241bfca
imkuldeepahlawat <imkuldeepahlawat@hotmail.com>
2022-12-07 14:25:06 +0530

    Linked list singy file updated

---
+++ linkedlist/singlell.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// for linked list
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+int main(){
+    node* node1 = new node(12);
+}
